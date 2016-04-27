f46f93b1ff2d2f6fd50cee218e2bd16f0b299dd9
Max F <max.floettmann@gmail.com>
2016-04-27 09:44:22 +0200

    correct example script

--- 01_introduction/example_script.py
+++ 01_introduction/example_script.py
@@ -3,6 +3,7 @@ import random
 
 passwords = {}
 pw_length = 8
+exit = False
 allowed_characters = string.ascii_letters + string.punctuation  + string.digits
 
 def generate_pw(n, allowed):
@@ -11,9 +12,12 @@ def generate_pw(n, allowed):
         pw += random.choice(allowed)
     return pw
 
-selection = input("(r)etrieve or (g)enerate password? ")
-if selection == 'r':
-    print(passwords.keys())
-    print(passwords[input("Which password would you like? ")])
-elif selection == 'g':
-    passwords[input("Enter account name: ")] = generate_pw(pw_length, allowed_characters)
+while not exit:
+    selection = input("(r)etrieve, (g)enerate password or (q)uit? ")
+    if selection == 'r':
+        print(passwords.keys())
+        print(passwords[input("Which password would you like? ")])
+    elif selection == 'g':
+        passwords[input("Enter account name: ")] = generate_pw(pw_length, allowed_characters)
+    elif selection == 'q':
+        exit = True
